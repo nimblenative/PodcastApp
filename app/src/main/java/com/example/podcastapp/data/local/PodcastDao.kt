c166604b690415cfee75a41eb362a147dce7338d
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-06-23 12:03:25 -0400

    data: Define queries for favorite entity and update database class

--- app/src/main/java/com/example/podcastapp/data/local/PodcastDao.kt
+++ app/src/main/java/com/example/podcastapp/data/local/PodcastDao.kt
@@ -2,8 +2,14 @@ package com.example.podcastapp.data.local
 
 import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
+import com.example.podcastapp.data.local.model.FavoriteEntity
+import com.example.podcastapp.data.local.model.PodcastEntity
+import com.example.podcastapp.data.local.model.PodcastWithFavoriteStatus
 import kotlinx.coroutines.flow.Flow
 
 /**
@@ -19,13 +25,33 @@ interface PodcastDao {
     @Query("DELETE FROM podcast")
     suspend fun clearAll()
 
-    @Query("SELECT * FROM podcast")
-    fun pagingSource(): PagingSource<Int, PodcastEntity>
+    @Transaction
+    @Query(
+        """
+       SELECT podcast.*, (favorite.podcastId IS NOT NULL) as isFavorite 
+       FROM podcast
+       LEFT JOIN favorite ON podcast.id = favorite.podcastId
+    """
+    )
+    fun pagingSource(): PagingSource<Int, PodcastWithFavoriteStatus>
 
     @Query("SELECT COUNT(*) FROM podcast")
     suspend fun getPodcastCount(): Int
 
-    @Query("SELECT * FROM podcast WHERE id = :id")
-    fun getPodcastById(id: String): Flow<PodcastEntity>
+    @Transaction
+    @Query(
+        """
+        SELECT podcast.*, (favorite.podcastId IS NOT NULL) as isFavorite
+        FROM podcast, favorite
+        WHERE id = :id
+    """
+    )
+    fun getPodcastById(id: String): Flow<PodcastWithFavoriteStatus>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertFavorite(favorite: FavoriteEntity)
+
+    @Query("DELETE FROM favorite WHERE podcastId = :podcastId")
+    suspend fun deleteFavorite(podcastId: String)
 
 }
--- app/src/main/java/com/example/podcastapp/data/local/PodcastDatabase.kt
+++ app/src/main/java/com/example/podcastapp/data/local/PodcastDatabase.kt
@@ -2,8 +2,14 @@ package com.example.podcastapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.podcastapp.data.local.model.FavoriteEntity
+import com.example.podcastapp.data.local.model.PodcastEntity
 
-@Database(entities = [PodcastEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [PodcastEntity::class, FavoriteEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class PodcastDatabase : RoomDatabase() {
 
     abstract fun podcastDao(): PodcastDao
