212f5accee0636d937c8f03e52fe2c34038f6cb6
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-06-23 12:16:13 -0400

    presentation: Define onFavoriteToggle method

--- app/src/main/java/com/example/podcastapp/presentation/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/podcastapp/presentation/details/DetailsViewModel.kt
@@ -5,16 +5,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.podcastapp.domain.model.Podcast
 import com.example.podcastapp.domain.use_case.GetPodcastByIdUseCase
+import com.example.podcastapp.domain.use_case.ToggleFavoriteUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailsViewModel @Inject constructor(
     getPodcastByIdUseCase: GetPodcastByIdUseCase,
-    savedStateHandle: SavedStateHandle
+    savedStateHandle: SavedStateHandle,
+    private val toggleFavoriteUseCase: ToggleFavoriteUseCase
 ) : ViewModel() {
 
     private val podcastId: String = checkNotNull(savedStateHandle["podcastId"])
@@ -25,4 +28,12 @@ class DetailsViewModel @Inject constructor(
             started = SharingStarted.WhileSubscribed(5_000L),
             initialValue = null
         )
+
+    fun onFavoriteToggle() {
+        viewModelScope.launch {
+            podcast.value?.let { currentPodcast ->
+                toggleFavoriteUseCase(currentPodcast.id, currentPodcast.isFavorite)
+            }
+        }
+    }
 }
