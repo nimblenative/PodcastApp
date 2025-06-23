1c51a8dbd5100531d905d2d598b0448ef68cb448
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-06-23 01:29:24 -0400

    presentation: Update viewModel and uiState class

--- app/src/main/java/com/example/podcastapp/presentation/home/HomeUiState.kt
+++ app/src/main/java/com/example/podcastapp/presentation/home/HomeUiState.kt
@@ -2,5 +2,6 @@ package com.example.podcastapp.presentation.home
 
 data class HomeUiState(
     val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
     val errorMessage: String? = null
 )
--- app/src/main/java/com/example/podcastapp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/podcastapp/presentation/home/HomeViewModel.kt
@@ -3,8 +3,9 @@ package com.example.podcastapp.presentation.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
+import com.example.podcastapp.domain.use_case.GetPodcastCountUseCase
 import com.example.podcastapp.domain.use_case.GetPodcastsUseCase
-import com.example.podcastapp.domain.use_case.RefreshPodcastsUseCase
+import com.example.podcastapp.domain.use_case.PagingPodcastsUseCase
 import com.example.podcastapp.domain.util.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,26 +17,47 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    getPodcastsUseCase: GetPodcastsUseCase,
-    private val refreshPodcastsUseCase: RefreshPodcastsUseCase
+    pagingPodcastsUseCase: PagingPodcastsUseCase,
+    private val getPodcastsUseCase: GetPodcastsUseCase,
+    private val getPodcastCountUseCase: GetPodcastCountUseCase
 ) : ViewModel() {
 
     // The stream of paginated data from the Paging 3 library
-    val podcasts = getPodcastsUseCase().cachedIn(viewModelScope)
+    val podcasts = pagingPodcastsUseCase().cachedIn(viewModelScope)
 
     // The mutable state for the UI
     private val _homeUiState = MutableStateFlow(HomeUiState())
     val homeUiState: StateFlow<HomeUiState> = _homeUiState.asStateFlow()
 
+    init {
+        initialLoadOrRefresh()
+    }
+
+    private fun initialLoadOrRefresh() {
+        viewModelScope.launch {
+            if (getPodcastCountUseCase() == 0) {
+                _homeUiState.update { it.copy(isLoading = true) }
+                val result = getPodcastsUseCase()
+                _homeUiState.update {
+                    it.copy(
+                        isLoading = false,
+                        errorMessage = if (result is Result.Error) result.message else null
+                    )
+                }
+            }
+        }
+    }
+
     fun refresh() {
         viewModelScope.launch {
-            _homeUiState.update { it.copy(isLoading = true) }
+            _homeUiState.update { it.copy(isRefreshing = true) }
 
-            when (val result = refreshPodcastsUseCase()) {
+            when (val result = getPodcastsUseCase()) {
                 is Result.Success -> {
                     _homeUiState.update {
                         it.copy(
                             isLoading = false,
+                            isRefreshing = false,
                             errorMessage = null
                         )
                     }
@@ -45,6 +67,7 @@ class HomeViewModel @Inject constructor(
                     _homeUiState.update {
                         it.copy(
                             isLoading = false,
+                            isRefreshing = false,
                             errorMessage = result.message
                         )
                     }
